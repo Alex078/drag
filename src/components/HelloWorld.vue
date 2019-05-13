85230f60a2d37482f752f3997c10cb2a2a836f1d
Alex078 <alexjakimchuk@gmail.com>
2019-05-13 18:52:50 +0300

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,116 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="row">
+        <div class="col-3">
+            <h3>Draggable 1</h3>
+            <draggable
+                    class="list-group"
+                    :list="list1"
+                    v-bind="dragOptions"
+                    group="people"
+                    @change="log">
+                <transition-group class="list-group-item">
+                    <li
+                            class="list-group-item"
+                            v-for="(element, index) in list1"
+                            :key="element.id"
+                    >
+                        {{ element.name }} {{ element.id }}
+                    </li>
+                </transition-group>
+            </draggable>
+        </div>
+
+        <div class="col-3">
+            <h3>Draggable 2</h3>
+            <draggable
+                    class="list-group"
+                    :list="list2"
+                    v-bind="dragOptions"
+                    group="people"
+                    @change="log">
+                <transition-group class="list-group-item">
+                    <li
+                            class="list-group-item"
+                            v-for="(element, index) in list2"
+                            :key="element.id"
+                    >
+                        {{ element.name }} {{ element.id }}
+                    </li>
+                </transition-group>
+            </draggable>
+        </div>
+
+        <div class="col-3">
+            <h3>Draggable 3</h3>
+            <draggable
+                    class="list-group"
+                    :list="list3"
+                    v-bind="dragOptions"
+                    group="people"
+                    @change="log">
+                <transition-group class="list-group-item">
+                    <li
+                            class="list-group-item"
+                            v-for="(element, index) in list3"
+                            :key="element.id"
+                    >
+                        {{ element.name }} {{ element.id }}
+                    </li>
+                </transition-group>
+            </draggable>
+        </div>
+    </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import draggable from "vuedraggable";
+    export default {
+        name: "two-lists",
+        order: 1,
+        components: {
+            draggable
+        },
+        data() {
+            return {
+                list1: [
+                    { name: "John", id: 1 },
+                    { name: "Joao", id: 2 },
+                    { name: "Jean", id: 3 },
+                    { name: "Gerard", id: 4 }
+                ],
+                list2: [
+                    { name: "Juan", id: 5 },
+                    { name: "Edgard", id: 6 },
+                    { name: "Johnson", id: 7 }
+                ],
+                list3: [
+                    { name: "Juanf", id: 8 },
+                    { name: "Edgarde", id: 9 },
+                    { name: "Johnsonrrt", id: 10 }
+                ]
+            };
+        },
+        methods: {
+            add: function() {
+                this.list.push({ name: "Juan" });
+            },
+            replace: function() {
+                this.list = [{ name: "Edgard" }];
+            },
+            clone: function(el) {
+                return {
+                    name: el.name + " cloned"
+                };
+            },
+            log: function(evt) {
+                window.console.log(evt);
+            }
+        },
+        computed: {
+            dragOptions() {
+                return {
+                    animation: 200
+                };
+            }
+        }
+    };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TwoLists msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import TwoLists from "../components/HelloWorld";
 
 export default {
   name: 'home',
   components: {
+    TwoLists,
     HelloWorld
   }
 }
